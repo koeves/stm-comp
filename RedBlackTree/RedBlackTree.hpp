5f8140fb5dfbe5ebf17df6c260104d310a93b295
Daniel Koves <daniel.koves@icloud.com>
2021-04-25 17:01:42 +0200

    finished insert and delete ops

--- RedBlackTree/RedBlackTree.hpp
+++ RedBlackTree/RedBlackTree.hpp
@@ -1,20 +1,23 @@
 #ifndef RED_BLACK_TREE_HPP
 #define RED_BLACK_TREE_HPP
 
+#define _BLACK  Node<T>::Color::BLACK 
+#define _RED    Node<T>::Color::RED  
+
 template<class T = int>
 class RedBlackTree { 
 public:
     RedBlackTree() {
         nil = new Node<T>;
-        nil->color = Node<T>::Color::BLACK;
+        nil->color = _BLACK;
         root = nil;
     }
 
     RedBlackTree(T x) {
         root = new Node<T>(x);
         nil = new Node<T>;
-        root->left = root->right = root->parent = nil;
-        root->color = nil->color = Node<T>::Color::BLACK;
+        root->l = root->r = root->p = nil->l = nil->r = nil->p = nil;
+        root->color = nil->color = _BLACK;
     }
 
     ~RedBlackTree() {
@@ -24,7 +27,7 @@ public:
 
     void insert(T x) { insert(new Node<T>(x)); }
 
-    void remove(T x);
+    void remove(T x) { remove(find(root, x)); }
 
     void print() { print_inorder(root); }
 
@@ -33,18 +36,18 @@ private:
     template<class U = int>
     struct Node {
         Node() {
-            left = right = parent = nullptr;
+            l = r = p = nullptr;
             key = -9999;
             color = Color::RED;
         }
 
         Node(U x) {
-            left = right = parent = nullptr;
+            l = r = p = nullptr;
             key = x;
             color = Color::RED;
         }
 
-        Node<U> *left, *right, *parent;
+        Node<U> *l, *r, *p;
         U key;
 
         enum class Color { RED, BLACK } color;
@@ -55,8 +58,8 @@ private:
     Node<T> *empty(Node<T> *t) {
         if (t == nil) return t;
 
-        empty(t->left);
-        empty(t->right);
+        empty(t->l);
+        empty(t->r);
         delete t;
 
         return nullptr;
@@ -65,32 +68,33 @@ private:
     void print_inorder(Node<T> *t) {
         if (t == nil) return;
 
-        print_inorder(t->left);
+        print_inorder(t->l);
         
-        std::cout << '(' << ((t->color == Node<T>::Color::BLACK) ? "BLACK" : "RED") << ")" 
-                  << t->key << " -> parent: " << t->parent->key << ", " 
-                  << "left: " << t->left->key << ", right: " << t->right->key
+        std::cout << ((t == root) ? "ROOT (" :  "     (")
+                  << ((t->color == _BLACK) ? "BLACK) " : "RED)   ")
+                  << t->key << " -> p: " << t->p->key << ", " 
+                  << "l: " << t->l->key << ", r: " << t->r->key
                   << std::endl;
 
-        print_inorder(t->right);
+        print_inorder(t->r);
     }
 
     Node<T> *find_min(Node<T> *t) {
-        if (t == nil) return nullptr;
-        else if (!t->left) return t;
-        else return find_min(t->left);
+        if (t == nil) return t;
+        else if (t->l == nil) return t;
+        else return find_min(t->l);
     }
 
     Node<T> *find_max(Node<T> *t) {
-        if (t == nil) return nullptr;
-        else if (!t->right) return t;
-        else return find_min(t->right);
+        if (t == nil) return t;
+        else if (t->r == nil) return t;
+        else return find_min(t->r);
     }
 
     Node<T> *find(Node<T> *t, T x) {
-        if(t == nil) return nullptr;
-        else if(x < t->key) return find(t->left, x);
-        else if(x > t->key) return find(t->right, x);
+        if(t == nil) return t;
+        else if(x < t->key) return find(t->l, x);
+        else if(x > t->key) return find(t->r, x);
         else return t;
     }
 
@@ -100,99 +104,197 @@ private:
 
         while (x != nil) {
             y = x;
-            if (z->key < x->key) x = x->left;
-            else x = x->right;
+            if (z->key < x->key) x = x->l;
+            else x = x->r;
         }
 
-        z->parent = y;
+        z->p = y;
 
         if (y == nil) root = z;
-        else if (z->key < y->key) y->left = z;
-        else y->right = z;
+        else if (z->key < y->key) y->l = z;
+        else y->r = z;
 
-        z->left = z->right = nil;
+        z->l = z->r = nil;
+        z->color = _RED;
 
         insert_fixup(z);
     }
 
     void insert_fixup(Node<T> *z) {
-        while (z->parent->color == Node<T>::Color::RED) {
-            if (z->parent == z->parent->parent->left) {
-                Node<T> *y = z->parent->parent->right;
-                if (y->color == Node<T>::Color::RED) {
-                    z->parent->color = y->color = Node<T>::Color::BLACK;
-                    z->parent->parent->color = Node<T>::Color::RED;
-                    z = z->parent->parent;
+        while (z->p->color == _RED) {
+            if (z->p == z->p->p->l) {
+                Node<T> *y = z->p->p->r;
+                if (y->color == _RED) {
+                    z->p->color = y->color = _BLACK;
+                    z->p->p->color = _RED;
+                    z = z->p->p;
                 }
-                else if (z == z->parent->right) {
-                    z = z->parent;
+                else if (z == z->p->r) {
+                    z = z->p;
                     left_rotate(z);
                 }
                 else {
-                    z->parent->color = Node<T>::Color::BLACK;
-                    z->parent->parent->color = Node<T>::Color::RED;
-                    right_rotate(z->parent->parent);
+                    z->p->color = _BLACK;
+                    z->p->p->color = _RED;
+                    right_rotate(z->p->p);
                 }
             }
             else {
-                Node<T> *y = z->parent->parent->left;
-                if (y->color == Node<T>::Color::RED) {
-                    z->parent->color = y->color = Node<T>::Color::BLACK;
-                    z->parent->parent->color = Node<T>::Color::RED;
-                    z = z->parent->parent;
+                Node<T> *y = z->p->p->l;
+                if (y->color == _RED) {
+                    z->p->color = y->color = _BLACK;
+                    z->p->p->color = _RED;
+                    z = z->p->p;
                 }
-                else if (z == z->parent->left) {
-                    z = z->parent;
+                else if (z == z->p->l) {
+                    z = z->p;
                     right_rotate(z);
                 }
                 else {
-                    z->parent->color = Node<T>::Color::BLACK;
-                    z->parent->parent->color = Node<T>::Color::RED;
-                    left_rotate(z->parent->parent);
+                    z->p->color = _BLACK;
+                    z->p->p->color = _RED;
+                    left_rotate(z->p->p);
                 }
             }
         }
-        root->color = Node<T>::Color::BLACK;
+        root->color = _BLACK;
+    }
+
+    void remove(Node<T> *z) {
+        Node<T> *y = z, *x;
+        enum Node<T>::Color y_orig_color = y->color;
+
+        if (z->l == nil) {
+            x = z->r;
+            transplant(z, z->r);
+        }
+        else if (z->r == nil) {
+            x = z->l;
+            transplant(z, z->l);
+        }
+        else {
+            y = find_min(z->r);
+            y_orig_color = y->color;
+            x = y->r;
+            if (y->p == z) {
+                x->p = y;
+            }
+            else {
+                transplant(y, y->r);
+                y->r = z->r;
+                y->r->p = y;
+            }
+            transplant(z, y);
+            y->l = z->l;
+            y->l->p = y;
+            y->color = z->color;
+        }
+
+        if (y_orig_color == _BLACK)
+            delete_fixup(x);
+
+        delete z;
+    }
+
+    void delete_fixup(Node<T> *x) {
+        while (x != root && x->color == _BLACK) {
+            if (x == x->p->l) {
+                Node<T> *w = x->p->r;
+                if (w->color == _RED) {
+                    w->color = _BLACK;
+                    x->p->color = _RED;
+                    left_rotate(x->p);
+                    w = x->p->r;
+                }
+
+                if (w->l->color == _BLACK && w->r->color == _BLACK) {
+                    w->color = _RED;
+                    x = x->p;
+                }
+                else if (w->r->color == _BLACK) {
+                    w->l->color = _BLACK;
+                    w->color = _RED;
+                    right_rotate(w);
+                    w = x->p->r;
+                }
+                else {
+                    w->color = x->p->color;
+                    x->p->color = _BLACK;
+                    w->r->color = _BLACK;
+                    left_rotate(x->p);
+                    x = root;
+                }
+            }
+            else {
+                Node<T> *w = x->p->l;
+                if (w->color == _RED) {
+                    w->color = _BLACK;
+                    x->p->color = _RED;
+                    right_rotate(x->p);
+                    w = x->p->l;
+                }
+
+                if (w->r->color == _BLACK && w->l->color == _BLACK) {
+                    w->color = _RED;
+                    x = x->p;
+                }
+                else if (w->l->color == _BLACK) {
+                    w->r->color = _BLACK;
+                    w->color = _RED;
+                    left_rotate(w);
+                    w = x->p->l;
+                }
+                else {
+                    w->color = x->p->color;
+                    x->p->color = _BLACK;
+                    w->l->color = _BLACK;
+                    right_rotate(x->p);
+                    x = root;
+                }
+            }
+        }
+        x->color = _BLACK;
+    }
+
+    void transplant(Node<T> *u, Node<T> *v) {
+        if (u->p == nil) root = v;
+        else if (u == u->p->l) u->p->l = v;
+        else u->p->r = v;
+        v->p = u->p;
     }
 
     void left_rotate(Node<T> *x) {
-        Node<T> *y = x->right;
-        x->right = y->left;
+        Node<T> *y = x->r;
+        x->r = y->l;
 
-        if (y->left != nil)
-            y->left->parent = x;
+        if (y->l != nil)
+            y->l->p = x;
 
-        y->parent = x->parent;
+        y->p = x->p;
 
-        if (x->parent == nil)
-            root = y;
-        else if (x == x->parent->left)
-            x->parent->left = y;
-        else
-            x->parent->right = y;
+        if (x->p == nil) root = y;
+        else if (x == x->p->l) x->p->l = y;
+        else x->p->r = y;
 
-        y->left = x;
-        x->parent = y;
+        y->l = x;
+        x->p = y;
     }
 
     void right_rotate(Node<T> *y) {
-        Node<T> *x = y->left;
-        y->left = x->right;
+        Node<T> *x = y->l;
+        y->l = x->r;
 
-        if (x->right != nil)
-            x->right->parent = y;
+        if (x->r != nil)
+            x->r->p = y;
 
-        x->parent = y->parent;
+        x->p = y->p;
 
-        if (y->parent == nil)
-            root = x;
-        else if (y == y->parent->right)
-            y->parent->right = x;
-        else
-            y->parent->left = x;
+        if (y->p == nil) root = x;
+        else if (y == y->p->r) y->p->r = x;
+        else y->p->l = x;
 
-        y->parent = x;
-        x->right = y;
+        y->p = x;
+        x->r = y;
     }
 };
 
