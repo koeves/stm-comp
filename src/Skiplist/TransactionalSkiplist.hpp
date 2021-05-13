81e1d2d7dd40e362e80c8aa814b2f3f60adef05c
danielkoves <daniel.koves@icloud.com>
2021-05-13 17:37:44 +0200

    started transactional skiplist

--- src/Skiplist/TransactionalSkiplist.hpp
+++ src/Skiplist/TransactionalSkiplist.hpp
@@ -2,6 +2,102 @@
 #define TX_SKIP_HPP
 
 template<class T = int>
-class TransactionalSkiplist {};
+class TransactionalSkiplist {
+public:
+
+    TransactionalSkiplist() : 
+        head(new SkiplistNode<T>(-1, MAX_LEVEL)), 
+        level(0) 
+    {}
+
+    ~TransactionalSkiplist() { 
+        for (int i = 0; i <= level; i++) {
+            SkiplistNode<T> *node = head->neighbours[i];
+            while (node != NULL) {
+                delete node;
+                node = node->neighbours[i];
+            }
+        }
+        delete head; 
+    }
+
+    bool contains(T val);
+
+    void add(T val);
+
+    bool remove(T val);
+
+    void display() {
+        for (int i = level; i >= 0; i--) {
+            SkiplistNode<T> *node = head->neighbours[i];
+            std::cout << "Level " << i << ": ";
+            while (node != NULL) {
+                std::cout << node->value << " ";
+                node = node->neighbours[i];
+            }
+            std::cout << "\n";
+        }
+    }
+
+private:
+
+    SkiplistNode<T> *head;
+    int level;
+
+    static constexpr double PROB = 0.5;
+    static const int MAX_LEVEL = 6;
+
+    double get_random_prob() {
+        std::random_device rd;
+        std::mt19937 mt(rd());
+        std::uniform_real_distribution<double> dist(0., 1.);
+
+        return dist(mt);
+    }
+
+    int get_random_height() {
+        double r = get_random_prob();
+        int lvl = 0;
+        while (r < PROB && lvl < MAX_LEVEL) {
+            lvl++;
+            r = get_random_prob();
+        }
+        return lvl;
+    }
+
+};
+
+template<class T>
+void TransactionalSkiplist<T>::add(T val) {
+    SkiplistNode<T> *curr = head;
+    SkiplistNode<T> *update[MAX_LEVEL + 1];
+    memset(update, 0, sizeof(SkiplistNode<T>*)*(MAX_LEVEL + 1));
+
+    for (int i = level; i >= 0; i--) {
+        while (curr->neighbours[i] != NULL && curr->neighbours[i]->value < val)
+            curr = curr->neighbours[i];
+        update[i] = curr;
+    }
+
+    curr = curr->neighbours[0];
+
+    if (curr == NULL || curr->value != val) {
+        int h = get_random_height();
+
+        if (h > level) {
+            for (int i = level + 1; i < h + 1; i++)
+                update[i] = head;
+
+            level = h;
+        }
+
+        SkiplistNode<T> *n = new SkiplistNode<T>(val, h);
+
+        for (int i = 0; i <= h; i++) {
+            n->neighbours[i] = update[i]->neighbours[i];
+            update[i]->neighbours[i] = n;
+        }
+    }
+}
 
 #endif
