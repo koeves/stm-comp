824a1537935a32b11a291aadec8a6a6776c346d7
Daniel Koves <daniel.koves@icloud.com>
2021-05-23 22:22:45 +0200

    added csv exporting

--- Test/runner.py
+++ Test/runner.py
@@ -1,17 +1,35 @@
 import time
 import subprocess
 import statistics
+import csv
 
 class Runner:
-    def __init__(self, executable):
+    def __init__(self, name):
         self.times = []
-        self.executable = executable
+        self.export = []
+        self.name = name
+        self.executable = './{}'.format(name)
+
 
     def run(self, times):
-        for _ in range(0, times):
-            start = time.time()
-            subprocess.call([self.executable], stdout=subprocess.DEVNULL)
-            self.times.append(round((time.time() - start) * 10 ** 6))
+        for _ in range(0, 100):
+            for _ in range(0, times):
+                start = time.time()
+                subprocess.call([self.executable], stdout=subprocess.DEVNULL)
+                self.times.append(round((time.time() - start) * 10 ** 6))
+
+            self.export.append(self.get_stats())
+            self.times.clear()
+
 
     def get_stats(self):
-        return statistics.mean(self.times), round(statistics.stdev(self.times), 2)
+        return [statistics.mean(self.times), round(statistics.stdev(self.times), 2)]
+
+
+    def write_csv(self):
+        with open('{}.csv'.format(self.name), 'w', newline='') as file:
+            fieldnames = ['mean', 'sd']
+            writer = csv.DictWriter(file, fieldnames=fieldnames)
+            writer.writeheader()
+            for row in self.export:
+                writer.writerow({'mean': row[0], 'sd': row[1]})
--- Test/test.py
+++ Test/test.py
@@ -4,6 +4,5 @@ from runner import Runner
 
 if __name__ == "__main__":
     r = Runner('./test_etx_1000')
-    r.run(100)
-    mean, sd = r.get_stats()
-    print("mean: {}\nsd: {}".format(mean, sd))
+    r.run(10)
+    r.write_csv()
